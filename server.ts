335539d01dc68820cb6115a48f84b6970d6ad70c
Matheus Vicente <vicentematheus1507@gmail.com>
2025-12-06 15:15:44 -0300

    feat: integrate database client and update courses endpoint to fetch data from the database

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
+import {db} from './src/database/client.ts'
+import {courses} from './src/database/schema.ts'
 
 const server = fastify({
     logger: {
@@ -13,53 +15,54 @@ const server = fastify({
     },
 })
 
-const courses = [ 
-    {id: '1', title: 'curso de Node.js'},
-    {id: '2', title: 'curso de React'},
-    {id: '3', title: 'curso de React Native'}
-]
+// const courses = [ 
+//     {id: '1', title: 'curso de Node.js'},
+//     {id: '2', title: 'curso de React'},
+//     {id: '3', title: 'curso de React Native'}
+// ]
  
 
 
-server.get('/courses', () => {
-    return {courses}
+server.get('/courses', async (request, reply) => {
+    const result = await db.select().from(courses)
+    return reply.send({courses: result})
 })
 
-server.get('/courses/:id', (request, reply) => {
-    type Params = {
-        id: string
-    }
+// server.get('/courses/:id', (request, reply) => {
+//     type Params = {
+//         id: string
+//     }
     
-    const params = request.params as Params
-    const courseId = params.id
+//     const params = request.params as Params
+//     const courseId = params.id
 
-    const course = courses.find(course => course.id === courseId)
+//     const course = courses.find(course => course.id === courseId)
 
-    if (course){
-        return {course}
-    }
+//     if (course){
+//         return {course}
+//     }
 
-    return reply.status(404).send()
-})
+//     return reply.status(404).send()
+// })
 
-server.post('/courses', (request, reply) => {
-    type Body = {
-        title: string
-    }
+// server.post('/courses', (request, reply) => {
+//     type Body = {
+//         title: string
+//     }
     
-    const courseId = crypto.randomUUID()
+//     const courseId = crypto.randomUUID()
 
-    const body = request.body as Body
-    const courseTitle = body.title
+//     const body = request.body as Body
+//     const courseTitle = body.title
 
-    if(!courseTitle){
-        return reply.status(400).send({message: 'Título obrigatório'})
-    }
+//     if(!courseTitle){
+//         return reply.status(400).send({message: 'Título obrigatório'})
+//     }
 
-    courses.push({id: courseId, title: courseTitle})
+//     courses.push({id: courseId, title: courseTitle})
 
-    return reply.status(201).send({courseId})
-})
+//     return reply.status(201).send({courseId})
+// })
 
 
 
